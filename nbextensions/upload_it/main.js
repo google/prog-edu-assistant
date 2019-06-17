9ac92a79abb700d389177d594489825892d09f8e
Salikh Zakirov <salikh@google.com>
2019-06-18 02:16:01 +0900

    Address review comments.

--- nbextensions/upload_it/main.js
+++ nbextensions/upload_it/main.js
@@ -86,10 +86,10 @@ define([
               method: "POST",
               success: function(data, status, jqXHR) {
                 // Open the report in a new tab.
-                var u = new URL(url);
-                var reportURL = u.protocol + "//" + u.host + data;
+                let reportURL = new URL(url);
+                reportURL.pathname = data;
+                window.console.log("Upload OK, opening report at ", reportURL.toString());
                 window.open(reportURL, '_blank');
-                window.console.log("Upload OK", reportURL);
               },
               error: function(jqXHR, status, err) {
                 if (err == "Unauthorized") {
