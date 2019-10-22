c81c9a5b0c80476c8d2d9c97450b3c2627b92b2b
Salikh Zakirov <salikh@google.com>
2019-10-23 00:51:21 +0900

    Reorganize docker files, create a new compbined image.

--- docker/README.md
+++ docker/README.md
@@ -1,5 +1,9 @@
 # Docker files
 
+There is a couple ways how you can run the autograder backend.
+
+## Separate workers and upload server with a message queue on Docker Compose
+
 To build the docker containers of upload server and autograder worker, use the
 command:
 
@@ -8,3 +12,13 @@ command:
 To run all services using Docker Compose:
 
     docker-compose up
+
+## A combined upload and grading server on Cloud Run
+
+To build the docker containers, use the command
+
+    ./build.sh
+
+To run the combined service, use the following Docker command:
+
+    docker
--- docker/build.sh
+++ docker/build.sh
@@ -12,23 +12,24 @@ function @execute() { echo "$@" >&2; "$@"; }
 
 DIR="$(dirname "$0")"
 DIR="$(cd "$DIR"; pwd -P)"
+STAGE="$DIR/stage"
 
 set -e
 
 @execute cd "$DIR/../"
 @execute bazel build ...
-@execute rm -rf "$DIR/worker/autograder"
-@execute tar xvf bazel-genfiles/exercises/autograder_image-layer.tar -C "$DIR/worker"
-@execute rm -rf "$DIR/worker/bin" && mkdir "$DIR/worker/bin"
-@execute cp -L "bazel-bin/go/cmd/worker/linux_amd64_stripped/worker" "$DIR/worker/bin"
-@execute rm -rf "$DIR/server/bin" && mkdir "$DIR/server/bin"
-@execute cp -L "bazel-bin/go/cmd/uploadserver/linux_amd64_stripped/uploadserver" "$DIR/server/bin"
-@execute rm -rf "$DIR/server/static"
-@execute cp -r "static" "$DIR/server/"
+@execute rm -rf "$STAGE/autograder"
+@execute tar xvf bazel-genfiles/exercises/autograder_image-layer.tar -C "$STAGE"
+@execute rm -rf "$STAGE/bin" && mkdir "$STAGE/bin"
+@execute cp -L "bazel-bin/go/cmd/worker/linux_amd64_stripped/worker" "$STAGE/bin"
+@execute cp -L "bazel-bin/go/cmd/uploadserver/linux_amd64_stripped/uploadserver" "$STAGE/bin"
+@execute rm -rf "$STAGE/static"
+@execute cp -r "static" "$STAGE/"
 
 @execute bazel run //go/cmd/uploadserver:docker -- --norun
 @execute bazel run //go/cmd/worker:docker -- --norun
 
 @execute cd "$DIR"
-@execute docker build server -t server
-@execute docker build worker -t worker
+@execute docker build stage -f stage/Dockerfile.server -t server
+@execute docker build stage -f stage/Dockerfile.worker -t worker
+@execute docker build stage -f stage/Dockerfile.combined -t combined
