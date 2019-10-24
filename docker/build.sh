0de91df813a7ab86f81fa70204313539056de725
Salikh Zakirov <salikh@google.com>
2019-10-25 04:18:16 +0900

    (tmp) Use a patched nsjail binary to workaround Cloud Run issue

--- docker/build.sh
+++ docker/build.sh
@@ -21,6 +21,7 @@ set -e
 @execute rm -rf "$STAGE/autograder"
 @execute tar xvf bazel-genfiles/exercises/autograder_image-layer.tar -C "$STAGE"
 @execute rm -rf "$STAGE/bin" && mkdir "$STAGE/bin"
+@execute cp -L "../nsjail/nsjail" "$STAGE/bin"
 @execute cp -L "bazel-bin/go/cmd/worker/linux_amd64_stripped/worker" "$STAGE/bin"
 @execute cp -L "bazel-bin/go/cmd/uploadserver/linux_amd64_stripped/uploadserver" "$STAGE/bin"
 @execute rm -rf "$STAGE/static"
