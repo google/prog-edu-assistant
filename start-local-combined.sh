10585012c78ae69a574059294a4ba4806c81701b
Salikh Zakirov <salikh@google.com>
2020-03-25 04:44:43 +0900

    start-local-combined.sh: Run in non-secure mode

--- start-local-combined.sh
+++ start-local-combined.sh
@@ -8,4 +8,13 @@ cd "$(dirname "$0")"
 bazel test ...
 cd docker
 ./build.sh
-docker run -p 8000:8000/tcp --rm --name combined combined:latest
+# Note: the following flags are appended to ENTRYPOINT defined in the Dockerfile.
+docker run -p 8000:8000/tcp --rm --name combined combined:latest \
+  --logtostderr \
+  --v=5 \
+  --disable_cleanup \
+  --auto_remove \
+  --log_to_bucket=0 \
+  --use_openid=0 \
+  --use_jwt=0 \
+  --secure_cookie=0
